4c722e50fdd9d2db7cb0a744345d49c9f3544a4c
Maksym741 <pastushinamaksim@gmail.com>
2024-03-15 19:37:02 +0100

    .

--- js/task-2.js
+++ js/task-2.js
@@ -14,6 +14,7 @@ console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); //
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+console.log(makeArray([]))//Виклик функції з випадковими масивами і випадковим числом повертає правильний масив
 
 
 //* Напиши функцію під назвою makeArray, яка приймає три параметри:
